f608b614b025f51b71794cc31ed96000b07aef23
Trevor Gould <goul0109@gmail.com>
2020-06-29 12:52:43 -0500

    update plot

--- R/sequence_count_plot.R
+++ R/sequence_count_plot.R
@@ -1,11 +1,11 @@
 #' @export
 # sequence count plot
-sct <- function(sequence_count_table){
+sequence_count_plot <- function(sct){
 #sequence_count_table <- read.delim("final_sequence_count.txt", row.names=1)
 #library(reshape2)
 #library(ggplot2)
 sct$Samples <- row.names(sct)
 melted <- reshape2::melt(sct)
-p <- ggplot2::ggplot(melted, aes(variable,value, fill = variable)) + theme_bw() + geom_boxplot() + geom_point() + geom_boxplot(alpha = 0.5) + geom_path(aes(group = Samples)) + xlab("Pipeline Stage") + ylab("Sequences") + theme(legend.position="none")
+p <- ggplot2::ggplot(melted, ggplot2::aes(variable,value, fill = variable)) + ggplot2::theme_bw() + ggplot2::geom_boxplot() + ggplot2::geom_point() + ggplot2::geom_boxplot(alpha = 0.5) + ggplot2::geom_path(ggplot2::aes(group = Samples)) + ggplot2::xlab("Pipeline Stage") + ggplot2::ylab("Sequences") + ggplot2::theme(legend.position="none")
 ggplot2::ggsave(p, file = "sequence_count_plot.png", dpi = 800, width = 6, height = 6, units = "in")
 }
