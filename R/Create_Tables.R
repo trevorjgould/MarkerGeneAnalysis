33e0dab3bc2e549578e69361016bc2456c911982
Trevor Gould <goul0109@gmail.com>
2020-11-09 14:45:13 -0600

    fixing taxonomy tables

--- R/Create_Tables.R
+++ R/Create_Tables.R
@@ -10,6 +10,7 @@
 #' @importFrom utils write.table
 
 Create_Tables <- function(inputtable,metadata,taxa){
+row.names(inputtable) <- gsub("-trimmed","", row.names(inputtable))
 #find common names
 common <- intersect(rownames(metadata),rownames(inputtable))
 # get just the overlapping samples
@@ -20,7 +21,12 @@ saveRDS(newtable, file = "Sequence_table_common.rds")
 write.table(newmap, file = "Metadata_common.txt")
 #Taxa table
 newtable1 <- t(newtable)
-both <- cbind(newtable1,taxa)
+#find common names
+common <- intersect(rownames(taxa),rownames(newtable1))
+# get just the overlapping samples
+taxa2 <- taxa[common,, drop = FALSE]
+newtable2 <- newtable1[common,, drop = FALSE]
+both <- cbind(newtable2,taxa2)
 #save to file
 write.table(both, file = "combined_sequences_taxa.txt", sep = "\t", quote = FALSE)
 return(list(newtable = newtable, newmap = newmap, combined_taxa = both))
